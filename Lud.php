a87dd1f5c2280c03e76fd14d2f93b54f2a42677e
Niklas Laxström <niklas.laxstrom@gmail.com>
2020-02-24 19:33:33 +0200

    Convert to extension.json

--- Lud.php
+++ Lud.php
@@ -10,19 +10,4 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die();
 }
 
-$GLOBALS['wgExtensionCredits']['other'][] = [
-	'path' => __FILE__,
-	'name' => 'Lud',
-	'version' => '2016-12-17',
-	'author' => 'Niklas Laxström',
-];
-
-$dir = __DIR__;
-require_once "$dir/Resources.php";
-
-$GLOBALS['wgMessagesDirs']['Lud'] = "$dir/i18n";
-
-$GLOBALS['wgHooks']['BeforePageDisplay'][] = function ( OutputPage $out ) {
-	$out->addModuleStyles( 'ext.sanat.styles' );
-	$out->addModules( 'ext.sanat' );
-};
+wfLoadExtension( 'Lud' );
---
+++ LudHooks.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Niklas Laxström
+ * @license GPL-2.0-or-later
+ */
+
+class LudHooks {
+	public static function onBeforePageDisplay( OutputPage $out ) {
+		$out->addModuleStyles( 'ext.lud.styles' );
+	}
+};
